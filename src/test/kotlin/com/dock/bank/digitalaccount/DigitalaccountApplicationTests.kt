d22cd1dc35d0877277ce45b61a7a76ad4ef869af
guilhermefreitas <guilherme.freitas@dock.tech>
2023-05-15 22:32:02 -0300

    commenting controllers test

--- src/test/kotlin/com/dock/bank/digitalaccount/DigitalaccountApplicationTests.kt
+++ src/test/kotlin/com/dock/bank/digitalaccount/DigitalaccountApplicationTests.kt
@@ -3,7 +3,7 @@ package com.dock.bank.digitalaccount
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-/*
+
 @SpringBootTest
 
 class DigitalaccountApplicationTests {
@@ -13,6 +13,3 @@ class DigitalaccountApplicationTests {
 	}
 
 }
-
-
- */
