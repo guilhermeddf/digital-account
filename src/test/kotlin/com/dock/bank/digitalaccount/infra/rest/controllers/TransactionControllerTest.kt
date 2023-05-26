862d27dcc16b3b9f51e19fe72d78e69eb7baccd5
Guilherme Freitas <guilhermedin@gmail.com>
2023-05-26 14:33:20 -0300

    Update TransactionControllerTest.kt

--- src/test/kotlin/com/dock/bank/digitalaccount/infra/rest/controllers/TransactionControllerTest.kt
+++ src/test/kotlin/com/dock/bank/digitalaccount/infra/rest/controllers/TransactionControllerTest.kt
@@ -1,4 +1,4 @@
-/* package com.dock.bank.digitalaccount.infra.rest.controllers
+package com.dock.bank.digitalaccount.infra.rest.controllers
 
 import com.dock.bank.digitalaccount.config.BaseTestConfig
 import com.dock.bank.digitalaccount.core.domain.TransactionType
@@ -136,4 +136,3 @@ class TransactionControllerTest : BaseTestConfig() {
     }
 
 }
- */
