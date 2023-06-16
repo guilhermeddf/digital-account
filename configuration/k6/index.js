610e6ae4d5b9d9bf9f043de1a387d26986dc9359
Guilherme Freitas <guilhermedin@gmail.com>
2023-06-16 00:36:41 -0300

    continuing multi module project

---
+++ configuration/k6/index.js
@@ -0,0 +1,31 @@
+import {group, sleep } from "k6/index"
+
+export const options = {
+    stages: [
+        // ramp-up from 1 to 5 VUs in 5s
+        { duration: '5s', target: 10 },
+
+        // stay at rest on 5 VUs for 30s
+        { duration: '30s', target: 15 },
+
+        // ramp-down from 5 to 0 VUs in 5s
+        { duration: '5s', target: 0 }
+    ],
+    thresholds: {
+        // throws error if more than 90% of the requests takes more than 2 seconds to be completed
+        http_req_duration: [
+            {
+                threshold: 'p(90) < 15000',
+                abortOnFail: true,
+                delayAbortEval: 100
+            }
+        ]
+    }
+}
+
+export default function() {
+    group('POSTACCOUNT - Controller Account', () => {
+        PostAccount();
+    });
+    sleep(1);
+}
